24416462e8e2d306a7c160fa0fe55271ae296353
robert schurig <rsc@mi24.tv>
2020-11-14 10:48:19 +0100

    upgrade to react 17

--- src/pages/Sports/index.js
+++ src/pages/Sports/index.js
@@ -1,5 +1,34 @@
 import React from 'react';
+import { useFetch } from 'hooks/useFetch';
 
 export const Sports = () => {
-  return <div>sports</div>;
+  const [imageList] = useFetch('data/istaf-2016.json');
+  const onClicked = id => {
+    alert(`fick: ${id}`);
+  };
+
+  return (
+    <div className="columns is-multiline">
+      {imageList.map(imageData => {
+        return (
+          <div
+            key={imageData.id}
+            className="column is-half-desktop is-one-tablet"
+          >
+            <div
+              className="box c-image-grid__item2"
+              onClick={() => onClicked(imageData.id)}
+            >
+              <figure className="image is-3by2">
+                <img src={imageData.source} alt="" />
+                <div className="overlay">
+                  <span className="text">hello</span>
+                </div>
+              </figure>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
